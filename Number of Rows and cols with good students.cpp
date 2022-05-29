81f5958dfece52b5cace5a92b7e8899f6eb37a05
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-05-30 02:29:41 +0600

    Create Number of Rows and cols with good students.cpp

---
+++ Number of Rows and cols with good students.cpp
@@ -0,0 +1,78 @@
+//starting with the name of almighty ALLAH
+///Author - RakibJoy
+// Practice is the only shortcut to improve
+//CF -1678D
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define dbg(x) cerr<<x<<endl;
+#define mp(x,y) make_pair(x,y)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define tst int t;cin>>t;while(t--)
+#define srt(v) sort(v.begin(),v.end());
+#define rsrt(v) sort(v.rbegin(),v.rend());
+#define rj ios::sync_with_stdio(false);cin.tie(0);
+#define rvs(v) reverse(v.begin(),v.end());
+#define F first
+#define S second
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define PI 2*acos(0.0)
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ld long double
+#define nl '\n'
+const int N=1e5;
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r)
+{
+    return uniform_int_distribution<int>(l, r) (rng);
+}
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    vl rw(b),cl(b);
+    ll cnt=0;
+    ll pre=-a*b;
+    for(ll i=0; i<a*b; i++)
+    {
+        char c;
+        cin>>c;
+        ll d=c-'0';
+        if(d)
+        {
+            pre=i;
+            if(!cl[i%b])
+                cnt++;
+            cl[i%b]++;
+        }
+        if(i-pre<b)
+        {
+            rw[i%b]++;
+        }
+        cout<<cnt+rw[i%b]<<' ';
+
+
+    }
+    cout<<endl;
+}
+int main()
+{
+    rj
+    tst
+    //int t;cin>>t;fr(i,1,t) cout<<"Case "<<i<<": ",solve();
+    //ll t;scanf("%lld",&t);fr(i,1,t) printf("Case %lld: ",i),solve();
+    solve();
+    return 0;
+}
